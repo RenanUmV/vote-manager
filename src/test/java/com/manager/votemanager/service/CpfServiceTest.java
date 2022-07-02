9b01606b35c91374b4413016dd6c97ece78831f6
RenanUmV <renan.uchoa.m@gmail.com>
2022-07-02 08:46:23 -0300

    Starting to implement tests

---
+++ src/test/java/com/manager/votemanager/service/CpfServiceTest.java
@@ -0,0 +1,50 @@
+package com.manager.votemanager.service;
+
+import com.manager.votemanager.config.CpfClient;
+import com.manager.votemanager.dto.CpfDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class CpfServiceTest {
+
+    @InjectMocks
+    private CpfService cpfService;
+
+    @Mock
+    CpfClient cpfClient;
+
+    private static final String VALID_CPF = "12345678910";
+
+    private static final String INVALID_CPF = "1234567891";
+
+
+    private CpfDto mockCpfDto(String cpf, boolean isValid) {
+        return new CpfDto(cpf, isValid);
+    }
+
+    @Test
+    void ShouldValidateCpfWhenCpfParamHas11Characters() {
+        when(cpfClient.validateCpf(VALID_CPF)).thenReturn(mockCpfDto(VALID_CPF, true));
+
+        boolean result = cpfService.validateCpf(VALID_CPF);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void ShouldValidateCpfWhenCpfParamHas10Characters() {
+        when(cpfClient.validateCpf(INVALID_CPF)).thenReturn(mockCpfDto(INVALID_CPF, false));
+
+        boolean result = cpfService.validateCpf(INVALID_CPF);
+
+        assertFalse(result);
+    }
+}
---
+++ src/test/java/com/manager/votemanager/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package com.manager.votemanager.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+
+}
