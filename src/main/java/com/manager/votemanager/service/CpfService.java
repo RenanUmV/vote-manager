7a6e4267a5e317410ebb109318a3ccf7e98b2abc
RenanUmV <renan.uchoa.m@gmail.com>
2022-07-10 23:09:23 -0300

    Including retry when consuming external api

--- src/main/java/com/manager/votemanager/service/CpfService.java
+++ src/main/java/com/manager/votemanager/service/CpfService.java
@@ -2,8 +2,12 @@ package com.manager.votemanager.service;
 
 
 import com.manager.votemanager.config.CpfClient;
+import com.manager.votemanager.dto.CpfDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,9 +21,22 @@ public class CpfService {
         this.cpfClient = cpfClient;
     }
 
+    @Retryable(maxAttempts = 3, backoff = @Backoff(3000))
     public boolean validateCpf(String cpf){
 
         log.info("Validating CPF");
         return cpfClient.validateCpf(cpf).getIsValid();
+    }
+
+    @Recover
+    public boolean saveCpf(String cpf){
+
+        CpfDto cpfDto = CpfDto.builder()
+                .cpf(cpf)
+                .isValid(true)
+                .build();
+
+        log.info("The service cannot be queried");
+        return cpfDto.getIsValid();
     }
 }
